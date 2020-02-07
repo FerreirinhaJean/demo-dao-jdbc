83d8dddecf6f816f657fd2d67b39b9ddc2a10537
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-02-06 21:40:12 -0300

    findAll implementation

--- src/applicantion/Program.java
+++ src/applicantion/Program.java
@@ -25,6 +25,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+
+		System.out.println("\n=== Test 3: Seller findByAll ===\n");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
